d35907904e1d515c3b20f411f5bf4165e0c980c7
Guillaume Dubois <dbmgui@gmail.com>
2017-10-30 18:28:03 +0100

    Ajout de champs d'option avec js

--- templates/creation.php
+++ templates/creation.php
@@ -73,34 +73,96 @@
 					<div class="form-group col-md-3" id="nom">
 						<input type="text" class="form-control" aria-describedby="sizing-addon2" name="nom_option">
 					</div>
-						<label class="col-form-label">Prix Option</label>
-						<div class="form-group col-sm-2">
-							<input type="number" class="form-control" aria-describedby="sizing-addon2" name="prix_option">
-						</div>
-						<label class="col-form-label">Nombre de place de l'option</label> <!-- max=max nombre total et uniquement si plusieurs options-->
-						<div class="form-group col-sm-2">
-							<input type="number" class="form-control" aria-describedby="sizing-addon2" name="place_option">
-						</div>	
-		   					<button type="button" class="btn btn-outline-secondary" style="padding: 15px; text-align: center;" onclick="addForm()"><font size=12>+</font></button>
-				<script>
+					<label class="col-form-label">Prix option</label>
+					<div class="form-group col-sm-2">
+						<input type="number" class="form-control" aria-describedby="sizing-addon2" name="prix_option">
+					</div>
+					<label class="col-form-label">Nombre de place de l'option</label> <!-- max=max nombre total et uniquement si plusieurs options-->
+					<div class="form-group col-sm-2">
+						<input type="number" class="form-control" aria-describedby="sizing-addon2" name="place_option">
+					</div>	
+   					<button type="button" class="btn btn-outline-secondary" style="padding: 15px; text-align: center;" onclick="addForm()"><font size=12>+</font></button>
+	   			</div>
+				<script>//oui c'est dégueu ici mais pour l'instant c'est plus pratique on le bougera plus tard
+					// var nboption = 1;
 					function addForm(){
-					var f = document.createElement("form");
-					f.setAttribute('method',"post");
-					f.setAttribute('action',"submit.php");
 
-					var i = document.createElement("input"); //input element, text
-					i.setAttribute('type',"text");
-					i.setAttribute('name',"username");
+						$("button").hide();//cache le bouton (provisoire, il faut afficher un bouton permettant de supprimer une option)
+						var f = document.createElement("form"); //creation nouveau formulaire (BESOIN?)
+						// f.setAttribute('method',"post");
+						// f.setAttribute('action',"submit.php");
+						var divrow = document.createElement("div");//mise en page pour que ça tienne sur 1 ligne
+						divrow.setAttribute('class',"form-row col-md-8");
 
-					f.appendChild(i);
+						var div1 = document.createElement("div");
+						div1.setAttribute('class',"form-group col-md-3");
 
-					//and some more input elements here
-					//and dont forget to add a submit button
+						var label1 = document.createElement("label"); //input element, text
+						label1.setAttribute('class',"col-form-label");
+						var text1 = document.createTextNode("Nom option #");//RAJOUTER COMPTEUR D'OPTION
+						label1.appendChild(text1)
 
-					document.getElementsByTagName('body')[0].appendChild(f);
+						var nom = document.createElement("input"); //input nom option
+						nom.setAttribute('type',"text");
+						nom.setAttribute('class',"form-control");
+						nom.setAttribute('name',"nom_option");
+						div1.appendChild(nom);
+
+						var div2 = document.createElement("div");
+						div2.setAttribute('class',"form-group col-sm-2");
+
+						var label2 = document.createElement("label"); //input element, text
+						label2.setAttribute('class',"col-form-label");
+						var text2 = document.createTextNode("Prix option");
+						label2.appendChild(text2)
+
+						var prix = document.createElement("input"); //input prix option
+						prix.setAttribute('class',"form-control");
+						prix.setAttribute('type',"number");
+						div2.appendChild(prix);
+
+						var div3 = document.createElement("div");
+						div3.setAttribute('class',"form-group col-sm-2");
+
+						var label3 = document.createElement("label"); //input element, text
+						label3.setAttribute('class',"col-form-label");
+						var text3 = document.createTextNode("Nombre de place de l'option");
+						label3.appendChild(text3)
+
+						var place = document.createElement("input"); //input nombre place option
+						place.setAttribute('class',"form-control");
+						place.setAttribute('type',"number");
+						div3.appendChild(place);
+
+						var bouton = document.createElement("button");
+						bouton.setAttribute('type',"button");
+						bouton.setAttribute('class',"btn btn-outline-secondary");
+						bouton.setAttribute('style',"padding: 15px; text-align: center;");
+						bouton.setAttribute('onclick',"addForm()");
+						var textBouton = document.createTextNode("+");//font size marche pas pour le "+" ici
+						bouton.appendChild(textBouton);
+
+						divrow.appendChild(label1);
+						divrow.appendChild(div1);
+						divrow.appendChild(label2);
+						divrow.appendChild(div2);
+						divrow.appendChild(label3);
+						divrow.appendChild(div3);
+						divrow.appendChild(bouton);
+						f.appendChild(divrow);
+
+						//and some more input elements here
+						//and dont forget to add a submit button
+
+						document.getElementsByTagName('body')[0].appendChild(f);
+						nom.focus();
+					}
+
+
+					function suppr_option(){
+
 					}
 				</script>
-			</div>
 
 		    
 		    <br>
