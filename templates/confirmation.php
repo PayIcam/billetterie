868b49103c6f1856c2f1950a902c0e4ee8d7764d
Unknown <gregoire.dervaux@2019.icam.fr>
2017-11-11 18:25:34 +0100

    ajout 1er essai BDD

--- templates/confirmation.php
+++ templates/confirmation.php
@@ -1,6 +1,25 @@
+
+
+<?php
+global $bdd;
+
+$ajout_shotgun=$bdd->prepare('INSERT INTO shutgun_desc(id_shotgun,nom_shotgun,date_debut,date_fin,descr,nb_place_tot,public_cible) 
+										VALUES(DEFAULT,:nom_shotgun,:date_debut,:date_fin,:descr,:nb_place_tot,:public_cible)');
+
+$ajout_shotgun->bindParam('nom_shotgun', $nom, PDO::PARAM_STR);
+$ajout_shotgun->bindParam('date_debut', $date_debut);
+$ajout_shotgun->bindParam('date_fin', $date_fin);
+$ajout_shotgun->bindParam('descr', $descr, PDO::PARAM_STR);
+$ajout_shotgun->bindParam('nb_place_tot', $nb_place_tot, PDO::PARAM_INT);
+$ajout_shotgun->bindParam('public_cible', $nom, PDO::PARAM_STR);
+
+$ajout_shotgun->execute();
+?>
+
 <html>
 
 	<body>
 		<h1><?php var_dump($_POST) ?></h1>
 	</body>
 </html>
+
