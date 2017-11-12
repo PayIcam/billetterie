091ed437ab90dba0d5cf3227bc1a7dd444bb41a4
Unknown <gregoire.dervaux@2019.icam.fr>
2017-11-12 17:34:23 +0100

    mise en place du public cible sur la page d'acceuil

--- templates/enregistrement.php
+++ templates/enregistrement.php
@@ -39,8 +39,13 @@ $ajout_shotgun=$bdd->prepare('INSERT INTO shotgun_desc(id_shotgun,nom_shotgun,da
 										VALUES(DEFAULT,:nom_shotgun,:date_debut,:date_fin,:descr,:nb_place_tot,:public_cible)');
 
 $ajout_shotgun->bindParam('nom_shotgun', $_POST['nom_shotgun'], PDO::PARAM_STR);
-$ajout_shotgun->bindParam('date_debut', $_POST['date_debut']);
-$ajout_shotgun->bindParam('date_fin', $_POST['date_fin']);
+
+$date_debut_UE = date('Y-m-d H:i:s', strtotime(str_replace('/', '-', $_POST['date_debut'])));
+$ajout_shotgun->bindParam('date_debut', $date_debut_UE, PDO::PARAM_STR);
+
+$date_fin_UE = date('Y-m-d H:i:s', strtotime(str_replace('/', '-', $_POST['date_fin'])));
+$ajout_shotgun->bindParam('date_fin', $date_fin_UE, PDO::PARAM_STR);
+
 $ajout_shotgun->bindParam('descr', $_POST['descr'], PDO::PARAM_STR);
 $ajout_shotgun->bindParam('nb_place_tot', $_POST['nb_place_tot'], PDO::PARAM_INT);
 $ajout_shotgun->bindParam('public_cible', $_POST['public_cible'], PDO::PARAM_STR);
