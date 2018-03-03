73c8691d66868782c70441a6e9d2ad7eacb3cad2
GregoireGiraud <gregoiregiraud1@gmail.com>
2018-03-03 14:20:33 +0100

    fichiers de création de base de données

---
+++ formulaire_billeterie/db_creation/db_insertions.sql
@@ -0,0 +1,4 @@
+INSERT INTO `ticketing`.`promos`(promo_name, still_student) VALUES
+(113,0), (114,0), (115,0), (116,0), (117,0), (2013,0), (2014,0), (2015,0), (2016,0), (2017,0), (118,1), (119,1), (120,1), (121,1), (122,1), (2018,1), (2019,1), (2020,1), (2021,1), (2022,1);
+
+INSERT INTO `ticketing`.`sites`(site_name) VALUES ('Lille'), ('Toulouse');
