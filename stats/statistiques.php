4c6bdef1eac22113b17b3e43d9c3e0b719fd079b
GregoireGiraud <gregoiregiraud1@gmail.com>
2018-04-15 17:16:40 +0200

    Plus parlant de comparer au quota actuel

--- stats/statistiques.php
+++ stats/statistiques.php
@@ -17,10 +17,10 @@ if(isset($_GET['event_id']))
 
         $total_quota = $event_details_stats['total_quota'];
         $event_details_stats['pourcentage_inscriptions'] = $total_quota !=0 ? round(100 * $event_details_stats['total_count'] / $total_quota, 2) . '%' : "undefined";
-        $event_details_stats['pourcentage_student'] = $event_details_stats['student_quota'] !=0 ? round(100 * $event_details_stats['student_count'] / $event_details_stats['student_quota'], 2) . '%' : "undefined";
-        $event_details_stats['pourcentage_graduated'] = $event_details_stats['graduated_quota'] !=0 ? round($event_details_stats['graduated_count'] / $event_details_stats['graduated_quota'], 2) . '%' : "undefined";
-        $event_details_stats['pourcentage_guests'] = $event_details_stats['guest_quota'] !=0 ? round(100 * $event_details_stats['guests_count'] / $event_details_stats['guest_quota'], 2) . '%' : "undefined";
-        $event_details_stats['pourcentage_bracelets'] = $event_details_stats['total_count'] !=0 ? round($event_details_stats['total_bracelet_count'] / $event_details_stats['total_count'], 2) . '%' : "undefined";
+        $event_details_stats['pourcentage_student'] = $total_quota !=0 ? round(100 * $event_details_stats['student_count'] / $total_quota, 2) . '%' : "undefined";
+        $event_details_stats['pourcentage_graduated'] = $total_quota !=0 ? round($event_details_stats['graduated_count'] / $total_quota, 2) . '%' : "undefined";
+        $event_details_stats['pourcentage_guests'] = $total_quota !=0 ? round(100 * $event_details_stats['guests_count'] / $total_quota, 2) . '%' : "undefined";
+        $event_details_stats['pourcentage_bracelets'] = $event_details_stats['total_count'] !=0 ? round(100 * $event_details_stats['total_bracelet_count'] / $event_details_stats['total_count'], 2) . '%' : "undefined";
 
         $event_days_stats = get_event_days_stats($event_id);
         $promo_specification_details_stats = get_promo_specification_details_stats($event_id);
